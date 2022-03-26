8aab63d30106df5babbdfedfb081956a96c43b2d
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-25 19:37:13 -0500

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -1,24 +1,26 @@
 import isDefined from 'defined.js';
 import { UNIX, delay } from 'time.js';
-import { Script, ScriptHandler } from './scripts.js';
+import blocks from 'blocks.js';
+const Costume = (name, src) => ({name: name, src: src});
 class Sprite {
-  // name can also be an object
-  constructor (name, costumes, x, y, dir, size, visible) {
-    if (typeof name === 'object') {
-      costumes = name.costumes;
-      x = name.position.x;
-      y = name.position.y;
-      dir = name.direction;
-      size = name.size;
-      visible = name.isVisible;
-      name = name.name;
+  // display can also be an object
+  constructor (display, name, x, y, dir, size, costumes, visible) {
+    if (typeof display === 'object') {
+      costumes = display.costumes;
+      x = display.position.x;
+      y = display.position.y;
+      dir = display.direction;
+      size = display.size;
+      visible = display.isVisible;
+      name = display.name;
+      display = display.display;
     }
     x = isDefined(x) ? x : 0;
     y = isDefined(y) ? y : 0;
     dir = isDefined(dir) ? dir : 90;
     size = isDefined(size) ? size : 100;
     visible = isDefined(visible) ? visible : true;
-    costumes = isDefined(costumes) ? costumes : [new Costume('costume1', 'https://commons.wikimedia.org/wiki/File:Cards-Blank.svg')];
+    costumes = isDefined(costumes) ? costumes : [Costume('costume1', 'https://commons.wikimedia.org/wiki/File:Cards-Blank.svg')];
     self.name = name;
     self.costumes = costumes;
     self.pos = {x: x, y: y};
@@ -29,7 +31,9 @@ class Sprite {
     self._displayTimestamp = NaN;
     self._displayDuration = NaN;
     self._messageDisplayType = undefined;
-    self.costume = {number: 1, name: self.costumes[0].name};
+    self.costume = {number: 0, name: self.costumes[0].name};
+    self._canvas = display;
+    self.layer = display.frontLayer;
     self.scripts = [];
   }
   show () {
@@ -38,15 +42,11 @@ class Sprite {
   hide () {
     self.isVisible = false;
   }
-  wait (seconds) {
-    self.scriptHandler.exec()
-    delay('seconds', seconds);
-  }
   _setMesssage (message, seconds, wait) {
     self.message = message;
     self._displayTimestamp = isDefined(seconds) ? UNIX('seconds') : NaN;
     self._displayDuration = isDefined(seconds) ? seconds : NaN;
-    if (wait && isDefined(seconds)) self.wait(seconds, script_id);
+    if (wait && isDefined(seconds)) blocks.control.wait(seconds);
   }
   say (message, seconds) {
     self._messageDisplayType = 'say';
@@ -57,6 +57,28 @@ class Sprite {
     self._setMessage(message, seconds, true);
   }
   setCostume (costume) {
-    self.display = 
+    if (typeof costume === 'number') {
+      self.costume = {number: costume, name: self.costumes[costume].name};
+    }
+    if (typeof costume === 'string') {
+      self.costume = {number: self.costumes.indexOf(costume), name: costume};
+    }
+  }
+  changeCostume (number) {
+    let costume = (self.costume.number + number) % self.costumes.length;
+    if (number < 0) {
+      costume = self.costumes.length * (costume < 0) + costume;
+    }
+    self.setCostume(costume);
+  }
+  setSize (n) {
+    self.size = Math.max(n, 0);
+  }
+  changeSize (by) {
+    setSize(self.size + by);
+  }
+  layer (n) {
+    self.layer = self._canvas.layer(n);
   }
 }
+export { Sprite, Costume };
