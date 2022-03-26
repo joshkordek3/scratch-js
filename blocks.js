4f8549d7eab6d2b0e3bc08163c409428f573edea
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-26 12:02:44 -0500

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,10 +1,12 @@
 import Math from 'math.js';
+import delay from 'time.js';
+import { Function, Object } from 'cloning.js';
 const blocks = {
   sound: {
     
   },
   control: {
-    
+    wait: delay
   }
 }
 export blocks;
