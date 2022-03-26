52c56865ff01caf30fc6fba27e5cdd49db5d74fa
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-25 20:06:02 -0500

    Create math.js

---
+++ math.js
@@ -0,0 +1,4 @@
+Math.constrain = (val, min, max) => Math.min(max, Math.max(min, value));
+Math.degToRad = (deg) => (deg * Math.PI / 180);
+Math.radToDeg = (rad) => (rad * 180 / Math.PI);
+export Math;
