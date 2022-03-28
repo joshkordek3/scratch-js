b77b7fe5f63e146a672d102188a671e501f2b3ea
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-28 11:13:07 -0500

    Update cloning.js

--- cloning.js
+++ cloning.js
@@ -1,3 +1,3 @@
-Object.clone = (obj) => Object.assign(new obj.constructor(), obj);
-Function.executable = (func, args) => (() => wait(...args));
+Object.clone = (obj) => Object.assign(new obj.constructor(), {...obj});
+Function.executable = (func, args) => (() => func(...args));
 export { Object, Function };
