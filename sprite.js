2320dff694a745f2c4788874f9f675f1d1e8275e
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-28 12:01:27 -0500

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -1,19 +1,24 @@
 import isDefined from 'defined.js';
+import Function from 'cloning.js';
 import UNIX from 'time.js';
 import blocks from 'blocks.js';
-const Costume = (name, src) => ({name: name, src: src});
+const Costume = (name, src) => {
+  let img = new Image();
+  img.src = src;
+  return {name: name, src: src, _renderImage: img};
+};
 class Sprite {
   // display can also be an object
-  constructor (display, name, x, y, dir, size, costumes, visible) {
-    if (typeof display === 'object') {
-      costumes = display.costumes;
-      x = display.position.x;
-      y = display.position.y;
-      dir = display.direction;
-      size = display.size;
-      visible = display.isVisible;
-      name = display.name;
-      display = display.display;
+  constructor (layerHandler, name, x, y, dir, size, costumes, visible) {
+    if (typeof layerHandler === 'object') {
+      costumes = layerHandler.costumes;
+      x = layerHandler.position.x;
+      y = layerHandler.position.y;
+      dir = layerHandler.direction;
+      size = layerHandler.size;
+      visible = layerHandler.isVisible;
+      name = layerHandler.name;
+      layerHandler = layerHandler.display;
     }
     x = isDefined(x) ? x : 0;
     y = isDefined(y) ? y : 0;
@@ -28,57 +33,79 @@ class Sprite {
     this.size = size;
     this.direction = dir;
     this.message = '';
-    this._displayTimestamp = NaN;
-    this._displayDuration = NaN;
     this._messageDisplayType = undefined;
-    this.costume = {number: 0, name: this.costumes[0].name};
-    this._canvas = display;
-    this.layer = display.frontLayer;
+    this.setCostume(0);
+    this._layerHandler = layerHandler;
+    this.layer = layerHandler.layer('front');
     this.scripts = [];
   }
   show () {
     this.isVisible = true;
+    return this.isVisible();
   }
   hide () {
     this.isVisible = false;
+    return this.isVisible():
   }
   _setMesssage (message, seconds, wait) {
     this.message = message;
-    this._displayTimestamp = isDefined(seconds) ? UNIX('seconds') : NaN;
-    this._displayDuration = isDefined(seconds) ? seconds : NaN;
-    if (wait && isDefined(seconds)) blocks.control.wait(seconds);
+    if (isDefined(seconds)) {
+      let func = () => {
+        blocks.control.wait(seconds);
+        this.message = '';
+      };
+      if (wait) func();
+      else setTimeout(func, 0);
+    }
+    return this.message;
   }
-  say (message, seconds) {
+  say (message, seconds, async) {
     this._messageDisplayType = 'say';
-    this._setMessage(message, seconds, true);
+    return this.this._setMessage(message, seconds, !async);
   }
-  think (message, seconds) {
+  think (message, seconds, async) {
     this._messageDisplayType = 'think';
-    this._setMessage(message, seconds, true);
+    return this.this._setMessage(message, seconds, !async);
   }
   setCostume (costume) {
     if (typeof costume === 'number') {
-      this.costume = {number: costume, name: this.costumes[costume].name};
+      this.costume = this.costumes[Math.constrain(costume, 0, this.costumes.length];
     }
     if (typeof costume === 'string') {
-      this.costume = {number: this.costumes.indexOf(costume), name: costume};
+      return setCostume(this._getCostumeIndex(costume)]);
     }
+    return this.costume;
   }
   changeCostume (number) {
     let costume = (this.costume.number + number) % this.costumes.length;
     if (number < 0) {
       costume = this.costumes.length * (costume < 0) + costume;
     }
-    this.setCostume(costume);
+    return this.setCostume(costume);
   }
   setSize (n) {
     this.size = Math.max(n, 0);
+    return this.size;
   }
   changeSize (by) {
-    setSize(this.size + by);
+    return setSize(this.size + by);
   }
-  layer (n) {
-    this.layer = this._canvas.layer(n);
+  setLayer (n) {
+    if (n === 'front' || n === 'last') {
+      this.layer = this._layerHandler.layer(n);
+    } else {
+      this.layer = this._layerHandler.layer(Math.max(n, 0));
+    }
+    return this.layer;
+  }
+  static forever (func) {
+   this._appendScript(func, () => (true));
+  }
+  _appendScript (func, repeatWhile) {
+    this.scripts.push(Function(`return ${String(func)}`)());
+  }
+  appendScript (func, repeatWhile) {
+    this._appendScript(func, typeof repeatWhile === 'function' ? repeatWhile : () => (false));
   }
 }
 export { Sprite, Costume };
