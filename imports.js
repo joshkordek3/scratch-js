b9b75ff6d9b68a62ebbdbe19f34d34a3c4a937d4
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-30 06:45:26 -0500

    Update imports.js

--- imports.js
+++ imports.js
@@ -1,3 +1,5 @@
 import blocks from 'blocks.js';
 import { Sprite, Costume } from 'sprite.js';
 import Display from 'display.js';
+import LayerHandler from 'layer-handler.js';
+export { blocks, Sprite, Costume, Display, LayerHandler };
